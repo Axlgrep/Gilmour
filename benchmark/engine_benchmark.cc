a5213c3c6616b42500ba7d364404ba3a20ba272a
Axlgrep <wuxianjian-iri@360.com>
2018-03-15 16:25:50 +0800

    add benchmark Makefile

---
+++ benchmark/engine_benchmark.cc
@@ -0,0 +1,17 @@
+//  Copyright (c) 2017-present The gilmour Authors.  All rights reserved.
+//  This source code is licensed under the BSD-style license found in the
+//  LICENSE file in the root directory of this source tree. An additional grant
+//  of patent rights can be found in the PATENTS file in the same directory.
+
+#include <iostream>
+#include <vector>
+#include <thread>
+#include <functional>
+
+#include "blackwidow/blackwidow.h"
+
+using namespace blackwidow;
+using namespace std::chrono;
+
+int main() {
+}
